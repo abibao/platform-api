0553569d1bf4a23b85de02171b2a8d7ac1a154e2
Gilles Perreymond <gperreymond@viadeoteam.com>
2017-06-16 17:43:53 +0200

    test standard update

--- test/standard.test.js
+++ test/standard.test.js
@@ -2,6 +2,6 @@ const standard = require('mocha-standard')
 
 describe('[standard] code', function () {
   it('should be conforms to standard', standard.files([
-    'config/*.js', 'test/**/*.js', 'test/**/*.js'
+    'config/*.js', 'server/**/*.js', 'test/**/*.js'
   ]))
 })
